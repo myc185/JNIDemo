ba1020709e77d0b9dd0e3d06d453b659b4fd3722
moyc <moyc@bosma.com.cn>
2019-01-01 18:18:39 +0800

    OpenGl 纹理绘制
    Signed-off-by: moyc <moyc@bosma.com.cn>

--- OpenGLESDemo/app/src/main/java/com/bosma/openglesdemo/YCRender.java
+++ OpenGLESDemo/app/src/main/java/com/bosma/openglesdemo/YCRender.java
@@ -11,8 +11,11 @@
 package com.bosma.openglesdemo;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
+import android.opengl.GLUtils;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
@@ -33,15 +36,39 @@ public class YCRender implements GLSurfaceView.Renderer {
     private Context mContext;
     private int mProgram;
     private int avPosition;
+    private int afPosition;
+
 
     private final float[] vertexData = {
-            -1f, 0f,
-            0f, 1f,
-            1f, 0f
+
+            -1f, -1f,
+            1f, -1f,
+            -1f, 1f,
+            1f, 1f
     };
 
+    /***
+     * 纹理坐标点(要与顶点一一对应)
+     */
+    private final float[] textureData = {
+//            0f, 1f,
+//            1f, 1f,
+//            0f, 0f,
+//            1f, 0f
+
+
+            //倒放
+            1f, 0f,
+            0f, 0f,
+            1f, 1f,
+            0f, 1f
+    };
+
+
     private FloatBuffer vertexBuffer;
-    private int afColor;
+    private FloatBuffer textureBuffer;
+    private int textureid;
+
 
     public YCRender(Context context) {
         this.mContext = context;
@@ -50,18 +77,50 @@ public class YCRender implements GLSurfaceView.Renderer {
                 .asFloatBuffer()
                 .put(vertexData);
         vertexBuffer.position(0);
+
+        textureBuffer = ByteBuffer.allocateDirect(textureData.length * 4)
+                .order(ByteOrder.nativeOrder())
+                .asFloatBuffer()
+                .put(textureData);
+        textureBuffer.position(0);
+
     }
 
 
     @Override
     public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
-        String vertexSource= YCShaderUtil.readRawTxt(mContext, R.raw.vertex_shader);
-        String fragmentSource= YCShaderUtil.readRawTxt(mContext, R.raw.fragment_shader);
+        String vertexSource = YCShaderUtil.readRawTxt(mContext, R.raw.vertex_shader);
+        String fragmentSource = YCShaderUtil.readRawTxt(mContext, R.raw.fragment_shader);
         mProgram = YCShaderUtil.createProgram(vertexSource, fragmentSource);
-        if(mProgram > 0)
-        {
-            avPosition = GLES20.glGetAttribLocation(mProgram, "av_Position");
-            afColor = GLES20.glGetUniformLocation(mProgram, "af_Color");
+        if (mProgram > 0) {
+            avPosition = GLES20.glGetAttribLocation(mProgram, "av_Position");//顶点
+            afPosition = GLES20.glGetAttribLocation(mProgram, "af_Position");//纹理
+
+
+            //创建纹理
+            int[] textureIds = new int[1];
+            GLES20.glGenTextures(1, textureIds, 0);
+            if (textureIds[0] == 0) {
+                return;
+            }
+
+            //绑定纹理
+            textureid = textureIds[0];
+            GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textureid);
+
+            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_S, GLES20.GL_REPEAT);
+            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_T, GLES20.GL_REPEAT);
+            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR);
+            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR);
+
+            Bitmap bitmap = BitmapFactory.decodeResource(mContext.getResources(), R.mipmap.og);
+            if (bitmap == null) {
+                return;
+            }
+            GLUtils.texImage2D(GLES20.GL_TEXTURE_2D, 0, bitmap, 0);
+            bitmap.recycle();
+            bitmap = null;
+
         }
 
     }
@@ -77,13 +136,19 @@ public class YCRender implements GLSurfaceView.Renderer {
 
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
         //用什么颜色清屏，显示的时候就是什么背景的颜色
-//        GLES20.glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+        GLES20.glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 
         GLES20.glUseProgram(mProgram);
-        GLES20.glUniform4f(afColor, 1f, 0f, 0f, 1f);
+
+
         GLES20.glEnableVertexAttribArray(avPosition);
         GLES20.glVertexAttribPointer(avPosition, 2, GLES20.GL_FLOAT, false, 8, vertexBuffer);
-        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 3);
+
+        GLES20.glEnableVertexAttribArray(afPosition);
+        GLES20.glVertexAttribPointer(afPosition, 2, GLES20.GL_FLOAT, false, 8, textureBuffer);
+
+
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, 4);//两个三角形，6个顶点
 
     }
 }
